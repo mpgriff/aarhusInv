1853393ef6137789ec963425fe7ce43028403132
pm <153464262+pmc93@users.noreply.github.com>
2024-04-08 09:58:27 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+__version__='0.0'
+setup(
+    name='aarhusInv',
+    author='Paul McLachlan',
+    author_email='pm@geo.au.dk',
+    packages=find_packages('src'),
+    package_dir={'':'src'},
+    install_requires=['numpy', 'os', 'subprocess', 'time', 'glob', 'pandas'],
+    version=__version__,
+    license='MIT',
+    description='create and read files to interact with aarhus inv',
+    python_requires=">=3.8",
+)
